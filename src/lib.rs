f43959b40b026d7984c0a0dbc3393d21e97409de
bakaking <tatattai@yahoo.com>
2019-08-08 13:59:29 +0700

    Working

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-//! A template application for Fluence.
+extern crate ketos;
+
 use fluence::sdk::*;
+use ketos::Interpreter;
 
 fn init() {
     logger::WasmLogger::init_with_level(log::Level::Info).unwrap();
@@ -7,5 +9,16 @@ fn init() {
 
 #[invocation_handler(init_fn = init)]
 fn main(arg: String) -> String {
-    return arg
+    let interp = Interpreter::new();
+    let code = interp.compile_exprs(arg.as_str()).unwrap();
+    let mut rs = String::new();
+
+    for c in code {
+        match interp.execute(c) {
+            Ok(v) => rs.push_str(interp.format_value(&v).as_str()),
+            Err(e) => rs.push_str(interp.format_error(&e).as_str()),
+        }
+        rs.push('\n');
+    }
+    return rs;
 }
